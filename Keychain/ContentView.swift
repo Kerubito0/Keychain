93ed5b167c078cd5b344d827af9c80ba60517d99
TMS <munbai6666@gmail.com>
2021-10-12 09:26:28 +0900

    Initial Commit

---
+++ Keychain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Keychain
+//
+//  Created by 田中義男 on 2021/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Keychain/KeychainApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainApp.swift
+//  Keychain
+//
+//  Created by 田中義男 on 2021/10/12.
+//
+
+import SwiftUI
+
+@main
+struct KeychainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
